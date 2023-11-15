f110f1158a980317590dea5b6f7fd796f19baa1a
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2023-11-15 14:35:15 -0700

    access token check

--- backend/index.js
+++ backend/index.js
@@ -11,119 +11,101 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Initialize Supabase client
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SECRET_KEY;
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET_KEY);
 
-// Middlewares
 app.use(cors());
-app.use(bodyParser.json()); // Use bodyParser to parse JSON bodies
+app.use(bodyParser.json());
 
 console.log('Starting server...');
 
-// Test endpoint
 app.get('/', (req, res) => {
-  res.send('Auctioning System Backend is running!');
+  res.send('Server is running!');
 });
 
-// User Registration Endpoint
-app.post('/register', async (req, res) => {
+async function registerUser(req, res) {
   const { username, email, password } = req.body;
-
-  // Check if all required fields are provided
   if (!username || !email || !password) {
-    return res.status(400).send('Please provide username, email, and password');
+    return res.status(400).send('Missing required fields');
   }
 
   try {
-    // Attempt to create a new user in Supabase
-    let { user, error: signUpError } = await supabase.auth.signUp({
-      email,
-      password,
-    });
+    const { user, error: signUpError } = await supabase.auth.signUp({ email, password });
+    if (signUpError) throw signUpError;
 
-    // Log the response from Supabase signUp method
-    console.log('Supabase signUp response:', { user, signUpError });
+    // Do not insert user details into the `users` table here.
+    // Wait for email confirmation first.
 
-    // Handle any errors from the signUp process
-    if (signUpError) {
-      console.error('Sign up error:', signUpError);
-      throw signUpError;
-    }
-
-    // Check if the user object is valid
-    if (!user) {
-      throw new Error("User was not created.");
-    }
-
-    // Insert additional user details into your users table
-    const { data: userData, error: insertError } = await supabase
-      .from('users')
-      .insert([{ id: user.id, username, email }]);
-
-    // Log the response from inserting user into your database
-    console.log('User data insertion response:', { userData, insertError });
-
-    // Handle any errors from the data insertion process
-    if (insertError) {
-      console.error('User data insertion error:', insertError);
-      throw insertError;
-    }
-
-    // Send successful response back
-    res.status(201).send({ user: userData });
-
+    res.status(201).send({ message: "Check your email to confirm your account", user });
   } catch (error) {
-    // Log and send back any errors encountered during the registration process
     console.error('Registration error:', error);
-    res.status(500).send({ message: error.message, details: error.details });
+    res.status(500).send({ message: error.message });
   }
-});
+}
 
+app.post('/register', registerUser);
 
-
-
-// User Login Endpoint
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).send('Please provide email and password');
+async function confirmUser(req, res) {
+  const { token } = req.body;
+  if (!token) {
+    return res.status(400).send('Token is required');
   }
 
   try {
-    let { user, session, error } = await supabase.auth.signIn({
-      email,
-      password
-    });
+    const { user, error: confirmError } = await supabase.auth.api.getUser(token);
+    if (confirmError) throw confirmError;
 
+    const username = user.user_metadata.username;
+    const email = user.email;
+
+    const { data, error: insertError } = await supabase.from('users').insert([{ id: user.id, username, email }]);
+    if (insertError) throw insertError;
+
+    res.status(200).send("User confirmed successfully.");
+  } catch (error) {
+    console.error('Confirmation error:', error);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+app.post('/confirm', confirmUser);
+
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).send('Email and password are required');
+  }
+
+  try {
+    const { user, session, error } = await supabase.auth.signIn({ email, password });
     if (error) throw error;
 
-    if (!user) throw new Error('User does not exist.');
+    if (!user) {
+      return res.status(404).send('User not found or not confirmed');
+    }
 
-    res.status(200).send({ message: 'Login successful', session });
+    res.status(200).send({ session });
   } catch (error) {
-    console.error(error);
+    console.error('Login error:', error);
     res.status(401).send({ message: error.message });
   }
-});
+}
 
+app.post('/login', loginUser);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+async function fetchUsers(req, res) {
+  try {
+    const { data, error } = await supabase.from('users').select('*');
+    if (error) throw error;
 
-// Fetch Users Endpoint
-app.get('/users', async (req, res) => {
-  const { data, error } = await supabase
-    .from('users')
-    .select('*');
-
-  if (error) {
-    return res.status(500).send(error.message);
+    res.status(200).send(data);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send({ message: error.message });
   }
+}
 
-  res.status(200).send(data);
+app.get('/users', fetchUsers);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
