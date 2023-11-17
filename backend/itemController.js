1b6b22d1f1d69cb5dad8abe59dc69a83a26770c5
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2023-11-17 13:23:32 -0700

    ItemControl

--- backend/itemController.js
+++ backend/itemController.js
@@ -1,26 +1,85 @@
+const { createClient } = require('@supabase/supabase-js');
+
+// Initialize Supabase client
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET_KEY);
+
 const getItems = async (req, res) => {
-    // Logic to retrieve all items
-    res.send('Get all items');
+    try {
+        let { data, error } = await supabase
+            .from('items')
+            .select('*');
+        
+        if (error) throw error;
+        
+        res.json(data);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 const createItem = async (req, res) => {
-    // Logic to create a new item
-    res.send('Create an item');
+    const { seller_id, name, description, start_price, image_url } = req.body;
+    try {
+        let { data, error } = await supabase
+            .from('items')
+            .insert([{ seller_id, name, description, start_price, image_url }]);
+        
+        if (error) throw error;
+        
+        res.status(201).json(data[0]);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 const getItem = async (req, res) => {
-    // Logic to retrieve a single item by id
-    res.send('Get an item');
+    const { id } = req.params;
+    try {
+        let { data, error } = await supabase
+            .from('items')
+            .select('*')
+            .eq('id', id)
+            .single();
+        
+        if (error) throw error;
+        
+        res.json(data);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 const updateItem = async (req, res) => {
-    // Logic to update an item by id
-    res.send('Update an item');
+    const { id } = req.params;
+    const updates = req.body;
+    try {
+        let { data, error } = await supabase
+            .from('items')
+            .update(updates)
+            .eq('id', id);
+        
+        if (error) throw error;
+        
+        res.json(data[0]);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 const deleteItem = async (req, res) => {
-    // Logic to delete an item by id
-    res.send('Delete an item');
+    const { id } = req.params;
+    try {
+        let { data, error } = await supabase
+            .from('items')
+            .delete()
+            .eq('id', id);
+        
+        if (error) throw error;
+        
+        res.send({ message: "Item deleted successfully." });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 module.exports = {
