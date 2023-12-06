48a1c0f633069d8f0ecc2a1a1d652f575a4d13e4
silverview7 <ifeanyi.ekpe4@gmail.com>
2023-12-06 16:01:52 -0700

    FIx home display

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -35,7 +35,7 @@ function Dashboard() {
 
   return (
     <div className="dashboard-container">
-      <NavigationBar />
+      {/* <NavigationBar /> */}
       <nav className="dashboard-nav">
         <img src="/SparkBid.png" alt="SparkBid Logo" className="logo" />
         <button onClick={() => navigate("/home")}>
