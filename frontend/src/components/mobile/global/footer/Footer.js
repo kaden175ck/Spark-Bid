706d15e4c29f7f85763fb3e46f3c2d5b4757c062
silverview7 <victor@ifeanyis-mbp.ucalgary.ca>
2023-12-06 14:50:13 -0700

    Modified navigation bar

--- frontend/src/components/mobile/global/footer/Footer.js
+++ frontend/src/components/mobile/global/footer/Footer.js
@@ -4,24 +4,32 @@ import "./Footer.css";
 
 function Footer() {
   const navigate = useNavigate();
-
   return (
     <section className="Footer">
       <button onClick={() => navigate("/home")}>
-        <i className="fa-solid fa-house"></i>
-        Home
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <br />
+        Search
       </button>
       <button onClick={() => navigate("/my-listings")}>
         <i className="fa-solid fa-gavel"></i>
+        <br />
         Listings
       </button>
       <button onClick={() => navigate("/home")}>
-        <i className="fa-solid fa-money-bills"></i>
-        Bids
+        <i className="fa-solid fa-house"></i>
+        <br />
+        Home
       </button>
-      <button onClick={() => navigate("/home")}>
-        <i className="fa-solid fa-newspaper"></i>
-        Subscribed
+      <button onClick={() => navigate("/bids")}>
+        <i className="fa-solid fa-money-bills"></i>
+        <br />
+        Bidding
+      </button>
+      <button onClick={() => navigate("/profile")}>
+        <i className="fa-solid fa-user"></i>
+        <br />
+        Profile
       </button>
     </section>
   );
